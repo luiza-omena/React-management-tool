1892fa21bb4e564b127862993c7824517828afeb
luiza-omena <99267967+luiza-omena@users.noreply.github.com>
2023-08-27 11:35:53 -0300

    Update TaskBox.tsx
    Feature: Styling Task Boxes
    
    Fix: Refining Styles

--- task-manager/src/components/TaskBox.tsx
+++ task-manager/src/components/TaskBox.tsx
@@ -5,7 +5,26 @@ interface Props {
 }
 
 function TaskBox({ task }: Props) {
-  return <div>{task.content}</div>;
+  return (
+    <div
+      className="
+  bg-navy
+  p-2.5 h-[100px] 
+  min-h-[100px] 
+  items-center 
+  flex 
+  text-left 
+  text-grey
+  rounded-xl 
+  hover:ring-2 
+  hover:ring-inset 
+  hover:ring-rose-100 
+  cursor-grabs
+  "
+    >
+      {task.content}
+    </div>
+  );
 }
 
 export default TaskBox;
